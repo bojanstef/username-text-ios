9e8e20a2afbfe9f3c168c645612368a1a1d6cd95
Bojan Stefanović <5675392+bojanstef@users.noreply.github.com>
2024-12-18 15:56:43 -0500

    Initial Commit

---
+++ email-text/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  email-text
+//
+//  Created by Bojan Stefanović on 12/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
